fec7381be181a0c831b844068a693a9e69efe0dc
USER <gauthB80@outlook.com>
2023-01-13 12:01:49 +0100

    feat(src/app/_utils/globalStyle/*.scss) : ajout style global

---
+++ src/app/_utils/errors/errors.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-errors',
+  template: `
+    <p>
+      errors works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ErrorsComponent {
+
+}
